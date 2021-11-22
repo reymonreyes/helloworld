83bfb39c3f04fe4c5bc54f3a4f37dd40d10e315f
Reymon Reyes <rreyes@machship.com>
2021-11-22 20:08:45 +0800

    Initial

---
+++ HelloWorld/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HelloWorld
+{
+    class Program
+    {
+        //This is an entry point of the program
+        /*
+          this
+          is
+            a
+            comment          
+        */
+        static void Main(string[] args)
+        {
+
+            if (true)
+            {
+
+            }
+            var myName = "John";
+            var myname = "John";
+            Console.WriteLine("Hello World!");
+            Console.ReadLine();
+        }
+    }
+}
