d1f42fd4d596919e21ee1ad5289c523536c5a2e6
Reymon Reyes <rreyes@machship.com>
2021-11-30 20:52:47 +0800

    Arithmetic operators.

--- HelloWorld/Program.cs
+++ HelloWorld/Program.cs
@@ -9,31 +9,23 @@ namespace HelloWorld
     class Program
     {
         /*
-            VARIABLE & DATA TYPES
-            - Variable declaration
-            - Data types
-              - int
-              - float
-              - char
-              - string
-              - bool
+            ARITHMETIC OPERATORS
+            + addition
+            - subtraction
+            * multiplication
+            / division
         */
         static void Main(string[] args)
         {
-            var myName = "John";
-            int age = 12;
-            float height = 1.45F;
-            char middleInitial = 'D';
-            string favoriteFood = "Spaghetti";
-            bool doYouLikeDogs = true;
-
-            Console.WriteLine("Hello " + myName);
-            Console.WriteLine("You are " + age + " years old.");
-            Console.WriteLine("Your height is " + height + " meters.");
-            Console.WriteLine("Your middle initial is " + middleInitial + ".");
-            Console.WriteLine("Your favorite food is " + favoriteFood + ".");
-            Console.WriteLine("Do you like dogs? " + doYouLikeDogs);
-            Console.ReadKey();
+            var addition = 1+1;
+            var subtraction = 10-5;
+            var multiplication = 2*5;
+            var division = 10/2;
+            Console.WriteLine("Addition: 1+1 = " + addition);
+            Console.WriteLine("Subtraction: 10-5 = " + subtraction);
+            Console.WriteLine("Multiplication: 2*5 = " + multiplication);
+            Console.WriteLine("Division: 10/2 = " + division);
+            Console.ReadLine();
         }
     }
 }
